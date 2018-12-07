7a2e53c670b888c248791085ac7c773ebcdf6394
choujiojio <39364153+choujiojio@users.noreply.github.com>
2018-12-07 21:38:05 +0800

    wacf/index-添加计时脚本

---
+++ scripts/jsq.js
@@ -0,0 +1,22 @@
+function JSQ()
+{
+    var today = new Date();
+    var yyyy = today.getFullYear();
+    var mm = today.getMonth();
+    var dd = today.getDate();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    
+    // add a zero in front of numbers < 10
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('jsq').innerHTML = yyyy + "年" + mm + "月" + dd + "日" + " " + h + ":" + m + ":" + s;
+    t = setTimeout('JSQ()', 500);
+}
+
+function checkTime(i)
+{
+    if (i < 10) { i = "0" + i; }
+    return i;
+}

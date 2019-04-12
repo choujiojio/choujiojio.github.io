c331f716e1be5cd6689c35669754d271d39076d1
choujiojio <39364153+choujiojio@users.noreply.github.com>
2019-04-12 22:56:15 +0800

    添加了简单的计算日期间隔的脚本

--- scripts/jsq.js
+++ scripts/jsq.js
@@ -7,12 +7,21 @@ function JSQ()
     var h = today.getHours();
     var m = today.getMinutes();
     var s = today.getSeconds();
-    
+    var oldTime = new Date(Date.parse("4/26/2018"));
+    var endTime = new Date(Date.parse("4/26/2019"));
+    var diffTime = today - oldTime;
+    var dayOfWay = diffTime / 1000 / 60 / 60 / 24;
+    var deadline = endTime - today;
+    var dayOfDead = deadline / 1000 / 60 / 60 / 24;
     // add a zero in front of numbers < 10
     m = checkTime(m);
     s = checkTime(s);
     document.getElementById('jsq').innerHTML = yyyy + "年" + mm + "月" + dd + "日" + " " + h + ":" + m + ":" + s;
+    document.getElementById('dayofway').innerHTML = dayOfWay + "天";
+    document.getElementById('deadline').innerHTML = dayOfDead + "天";
     t = setTimeout('JSQ()', 500);
+
+
 }
 
 function checkTime(i)
